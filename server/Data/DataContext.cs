ce2400a21e14d75c243ea101d826e0a84c8b8edd
Tosmak <tosmak16@gmail.com>
2019-09-27 06:23:26 +0100

    Set database config and entityframework setup

---
+++ server/Data/DataContext.cs
@@ -0,0 +1,16 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace server.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder builder) { }
+
+
+    }
+}
--- server/Startup.cs
+++ server/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using server.Data;
 
 namespace server
 {
@@ -26,6 +28,7 @@ namespace server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("plaudernDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
