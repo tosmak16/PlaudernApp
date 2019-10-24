a0d14e8bac042d3338221ab9061a53590ff52fa4
Tosmak <tosmak16@gmail.com>
2019-10-24 12:16:55 +0100

    Create Loader component

---
+++ client/src/components/LoadingSpinner/LoadingSpinner.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-show="show" class="loading-ring">
+    <div />
+    <div />
+    <div />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "./LoadingSpinner.scss";
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+const LoadingSpinner = Vue.extend({
+  data() {
+    return {};
+  },
+
+  props: {
+    color: {
+      type: String,
+      default: "blue"
+    },
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {},
+  computed: {}
+});
+
+export default LoadingSpinner;
+</script>
