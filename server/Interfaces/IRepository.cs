82a6711613c637c8be61bdc87542159fd51e27e3
Tosmak <tosmak16@gmail.com>
2019-11-01 12:59:14 +0100

    Update repository

--- server/Interfaces/IRepository.cs
+++ server/Interfaces/IRepository.cs
@@ -7,7 +7,7 @@ namespace server.Interfaces
     public interface IRepository
     {
         Task<List<T>> FindAll<T>() where T : class;
-        Task<T> FindById<T>(long id) where T : class;
+        Task<T> FindById<T>(int id) where T : class;
         Task CreateAsync<T>(T entity) where T : class;
         Task UpdateAsync<T>(T entity) where T : class;
         Task DeleteAsync<T>(T entity) where T : class;
