ec68b6bb63f14b490f1c124ac9e6252ac0732529
Tosmak <tosmak16@gmail.com>
2019-11-01 12:58:46 +0100

    Add user details dto

--- server/Dtos/UserDetailDto.cs
+++ server/Dtos/UserDetailDto.cs
@@ -5,17 +5,15 @@ namespace server.Dtos
     public class UserDetailDto
     {
         public int Id { get; set; }
-
         public string Username { get; set; }
-
         public string Gender { get; set; }
-
-        public DateTime DateOfBirth { get; set; }
-
-        public string City { get; set; }
-
-        public string Country { get; set; }
+        public int Age { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhotoUrl { get; set; }
+        public string AboutMe { get; set; }
+
     }
 }
